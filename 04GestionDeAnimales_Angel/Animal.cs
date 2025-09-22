facf3af0e4e7303fd69d1898e1633d8849af84ed
angelzafra <angelzafra35@gmail.com>
2025-09-22 20:49:12 +0200

    He creado la clase animal con su constructor

---
+++ 04GestionDeAnimales_Angel/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04GestionDeAnimales_Angel
+{
+    internal class Animal
+    {
+        public String nombre { get; set; }
+        public String raza { get; set; }
+        public int edad { get; set; }
+
+        public Animal(String nombre, String raza, int edad)
+        {
+            this.nombre = nombre;
+            this.raza = raza;
+            this.edad = edad;
+        }
+
+    }
+}
