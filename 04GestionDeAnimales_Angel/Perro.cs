06c29e53f03f5be21a364584bf980fd55115d26e
angelzafra <angelzafra35@gmail.com>
2025-09-22 22:41:18 +0200

    Creación de la clase Perro con su propio metodo

---
+++ 04GestionDeAnimales_Angel/Perro.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04GestionDeAnimales_Angel
+{
+    internal class Perro: Animal
+    {
+        public string nombre { get; set; }
+        public string raza { get; set; }
+        public int edad { get; set; }
+
+        public Perro(string nombre, String raza, int edad) 
+        {
+            this.nombre = nombre;
+            this.raza = raza;
+            this.edad = edad;
+        }
+
+        public void ladrar()
+        {
+            Console.WriteLine("Guau Guau");
+        }
+    }
+}
