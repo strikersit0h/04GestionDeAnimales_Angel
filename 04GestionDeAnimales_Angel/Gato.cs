c900d7c4864b5b88aa2ac8a98494dc6416bc3d67
angelzafra <angelzafra35@gmail.com>
2025-09-22 22:42:48 +0200

    Creación de la clase Gato

---
+++ 04GestionDeAnimales_Angel/Gato.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04GestionDeAnimales_Angel
+{
+    internal class Gato : Animal
+    {
+        public string nombre { get; set; }
+        public string raza { get; set; }
+        public int edad { get; set; }
+
+        public Gato(string nombre, String raza, int edad) // Constructor
+        {
+            this.nombre = nombre;
+            this.raza = raza;
+            this.edad = edad;
+        }
+
+        public void maullar()
+        {
+            Console.WriteLine("Miaaau");
+        }
+    }
+}
